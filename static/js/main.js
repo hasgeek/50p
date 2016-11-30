49406adf1befd81da7a48e503eec0a2c4b6f3321
Vidya Ramakrishnan <vidya@hasgeek.in>
2016-11-30 20:02:35 +0530

    Show proposals from funnel.

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,44 @@
+var parseProposalJson = function(json) {
+
+  console.log("length", json.proposals.length);
+
+  var proposal_ractive = new Ractive({
+    el: '#funnel-proposals',
+    template: '#proposals-wrapper',
+    data: {
+      proposals: json.proposals,
+      description_max_ch_count: 286,
+      truncateDescription: function(description) {
+        var max_character_count = this.get('description_max_ch_count');
+        if (description.length < max_character_count) {
+          return description;
+        } else {
+          return description.slice(0, max_character_count) + '<span>...</span>';
+        }
+      }
+    },
+    complete: function() {
+      var updateFontSize = function(elem) {
+        var fontStep = 1;
+        var parentWidth = $(elem).width();
+        var parentHeight = parseInt($(elem).css('max-height'), 10);
+        var childElem = $(elem).find('span');
+        while ((childElem.width() > parentWidth) || (childElem.height() > parentHeight)) {
+          childElem.css('font-size', parseInt(childElem.css('font-size'), 10) - fontStep + 'px');
+        }
+      }
+
+      $.each($('.card .title'), function(index, title){
+        updateFontSize(title);
+      });
+
+      $(".scroll-pane-content").css('width', json.proposals.length * 484 + 'px');
+
+      $('.scroll-pane').jScrollPane({showArrows: true});
+    }
+  });
+}
+
 $(document).ready(function() {
 
   $(window).scroll(function() {
@@ -24,5 +65,20 @@ $(document).ready(function() {
         ga('send', { hitType: 'event', eventCategory: 'click', eventAction: button, eventLabel: section});
       }
   });
+
+  // For proposals
+  var proposals_url = 'https://50p.talkfunnel.com/2017/json';
+
+  //If funnel-proposals divs is present on the page, then make the ajax call.
+  if(($('#funnel-proposals').length)) {
+    $.ajax({
+      type: 'GET',
+      dataType: 'jsonp',
+      url: proposals_url,
+      success: function(data) {
+        parseProposalJson(data);
+      }
+    });//eof ajax call
+  }
 
 });
