0aa2758b4af80ffdc5672a82b341a605f89c86dd
Vidya Ramakrishnan <vidya@hasgeek.in>
2016-12-09 17:11:02 +0530

    Remove console.log

--- static/js/main.js
+++ static/js/main.js
@@ -20,7 +20,6 @@ $(document).ready(function() {
     var card_width = $(elem).css('width');
     var card_margin = $(elem).css('margin-left');
     var card_total_width = parseInt(card_width, 10) + 2.5 * parseInt(card_margin, 10);
-    console.log("card_total_width", card_total_width);
     return card_total_width;
   };
 
